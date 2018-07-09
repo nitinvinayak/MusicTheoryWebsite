eb7932033623d08061899f73da84213197c1c769
Nitin Vinayak Agrawal <f20170415@gmail.com>
2018-07-09 18:42:28 +0530

    Now the code is Perfect

--- Html/FretboardNotes.js
+++ Html/FretboardNotes.js
@@ -5,25 +5,29 @@ function Notefunc()
     console.log(this.textContent)
     var content=this.textContent
     var note
+    var j=0
     for (note of [7,2,10,5,0,7]){
                   var num=note
                   var i=0
-                  var j=0
-                  while (i<=22){
-                              i=i+1
 
+                  while (i<=22){
+
+                              console.log(notes[num]===content);
+                              console.log(22*j+i);
                               if(notes[num]===content)
                               {
                               data.eq(23*j+i).html(content)
                               console.log(data.eq(22*j+i).textContent)
+
                               console.log(content);
                             }
                               num=num+1
                               if(num>=12){
                                           num=num-12
                                         }
-                              j=j+1
+                              i=i+1
                               }
+                              j=j+1
                 }
 
 }
