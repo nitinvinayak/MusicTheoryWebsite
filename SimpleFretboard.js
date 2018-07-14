4babc282a10c99f81f1a6ed1650042987f2125ce
Nitin Vinayak Agrawal <f20170415@gmail.com>
2018-07-14 23:24:00 +0530

    code still not working

--- SimpleFretboard.js
+++ SimpleFretboard.js
@@ -1,21 +1,17 @@
 notes=["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"]
 function Notefunc()
 {
-this.outerHTML=""
-
-
-
+this.textContent="pressed"
 var note
 var j=0
 for (note of [7,2,10,5,0,7]){
               var num=note
               var i=0
-
               while (i<=22){
-                          console.log(22*j+i);
-
-
-                          if (data.eq(23*j+i).innerHTML==""){
+                          var elem=data.eq(23*j+i)
+                          console.log(elem.textContent)
+                          if (elem.textContent){
+                          this.outerHTML=''
                           data.eq(23*j+i).html(notes[num])
                           console.log(data.eq(22*j+i).textContent)
                         }
